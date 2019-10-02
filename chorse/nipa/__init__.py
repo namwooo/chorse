fae78cde8a96dfb8603cc8a97c3505151fabeae8
Luca <luca@bluewhale.kr>
2019-10-02 20:22:31 +0900

    nipa csvify 수정

--- chorse/nipa/__init__.py
+++ chorse/nipa/__init__.py
@@ -13,9 +13,8 @@ def nipa_cli():
 @nipa_cli.command('csvify')
 @click.option('-s', '--resource-path', help='소스 경로')
 @click.option('-t', '--target-path', help='저장 경로')
-@click.option('-e', '--event', help='이벤트 이름(정상, 파손, 실신, 폭행, 돌진)')
-def csvify(source_path, target_path, event):
-    source_path = os.path.abspath(os.path.expanduser(source_path))
+def csvify(resource_path, target_path):
+    source_path = os.path.abspath(os.path.expanduser(resource_path))
     target_path = os.path.abspath(os.path.expanduser(target_path))
     with open(source_path, 'r') as f:
         urls = f.read().split()
@@ -24,13 +23,13 @@ def csvify(source_path, target_path, event):
             csv_writer.writerow(
                 ['meta_info.video.url', 'meta_info.extract_info.quality', 'meta_info.extract_info.resize_width',
                  'meta_info.extract_info.extract_per_seconds',
-                 'meta_info.extract_info.extract_per_frames'])
+                 'meta_info.extract_info.extract_per_frames',
+                 'meta_info.extract_info.extract_count'])
             for url in urls:
                 parsed_url = parse.urlparse(url)
-                if event in parsed_url.path:
-                    encoded_url = parse.quote(parsed_url.path)
-                    result = 'https://nas-web-01.bluewhale.kr' + encoded_url
-                    csv_writer.writerow([result, 100, 0, 0, None])
+                encoded_path = parse.quote(parsed_url.path)
+                result = parsed_url.scheme + '://' + parsed_url.netloc + encoded_path
+                csv_writer.writerow([result, 100, 0, None, None, 500])
 
 
 
